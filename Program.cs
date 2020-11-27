f2009555247f4ab2db6ac1f3e34ff136c7b98cd3
Prado-SCamila <prado.camila.910423@gmail.com>
2020-11-27 13:09:14 -0300

    Projeto finalizado

--- Program.cs
+++ Program.cs
@@ -20,8 +20,7 @@ namespace Cadastro_Produto
              Console.WriteLine("[2] - Listar Produtos");
              Console.WriteLine("[3] - Verificar promoção]");   
              num = int.Parse(Console.ReadLine());
-  
-    
+      
         if(num == 1){
            
             Console.WriteLine("Deseja cadastrar um novo produto?");
@@ -60,8 +59,36 @@ namespace Cadastro_Produto
              num = int.Parse(Console.ReadLine());
 
         }else if(num == 3){
-
+            
             Console.WriteLine("Verificando promoção");
+            Console.WriteLine("Escolha o número referente ao produto desejado:");
+
+            Console.WriteLine("[0] -" +nome[0]);
+               Console.WriteLine("[1]-"+nome[1]);
+                  Console.WriteLine("[2]-"+nome[2]);
+                     Console.WriteLine("[3]-"+nome[3]);
+                        Console.WriteLine("[4]-"+nome[4]);
+                           Console.WriteLine("[5]-"+nome[5]);
+                              Console.WriteLine("[6]-"+nome[6]);
+                                 Console.WriteLine("[7]-"+nome[7]);
+                                    Console.WriteLine("[8]-"+nome[8]);
+                                       Console.WriteLine("[9]-"+nome[9]);
+            int escolha = int.Parse(Console.ReadLine());
+
+            Promocao(preco);
+        
+           
+            bool Promocao(float[] preco){
+                var i = escolha;
+                if(preco[i] < 50){
+                    Console.WriteLine("Está em promoção!");
+                    return true;
+                }else{
+                    Console.WriteLine("Não está em promoção");
+                    return false;
+                }
+            }
+        
                        
             }else{
             Console.WriteLine("O que você deseja fazer?");
